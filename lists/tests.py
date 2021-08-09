7c684acefbe4134b8df8b2865486534598a1f271
Luis Carvalho <luisfccarvalho00@gmail.com>
2021-08-08 23:19:36 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 2

--- lists/tests.py
+++ lists/tests.py
@@ -29,36 +29,35 @@ class HomePageTest(TestCase):
       self.client.get('/')
       self.assertEqual(Item.objects.count(), 0)
       
-    def test_displays_all_list_items(self):
-      Item.objects.create(text='itemey 1')
-      Item.objects.create(text='itemey 2')
-
-      response = self.client.get('/')
-
-      self.assertIn('itemey 1', response.content.decode())
-      self.assertIn('itemey 2', response.content.decode())
-
-
 from lists.models import Item
 ...
 
 class ItemModelTest(TestCase):
 
-    def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = 'O primeiro item'
-        first_item.save()
+  def test_saving_and_retrieving_items(self):
+    first_item = Item()
+    first_item.text = 'O primeiro item'      
+    first_item.save()
 
-        second_item = Item()
-        second_item.text = 'O segundo item'
-        second_item.save()
+    second_item = Item()
+    second_item.text = 'O segundo item'
+    second_item.save()
 
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
+    saved_items = Item.objects.all()
+    self.assertEqual(saved_items.count(), 2)
 
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'O primeiro item')
-        self.assertEqual(second_saved_item.text, 'O segundo item')
+    first_saved_item = saved_items[0]
+    second_saved_item = saved_items[1]
+    self.assertEqual(first_saved_item.text, 'O primeiro item')
+    self.assertEqual(second_saved_item.text, 'O segundo item')
 
+class ListViewTest(TestCase):
 
+  def test_displays_all_items(self):
+    Item.objects.create(text='itemey 1')
+    Item.objects.create(text='itemey 2')
+
+    response = self.client.get('/lists/the-only-list-in-the-world/')
+
+    self.assertContains(response, 'itemey 1')
+    self.assertContains(response, 'itemey 2')
